3c4c7ee970c8d4a2fe79ef02394daedff5018641
Haseeb Majid <hmajid2301@gmail.com>
2019-04-11 10:34:46 +0100

    Delete ColourChange.js

--- js/ColourChange.js
+++
@@ -1,7 +0,0 @@
-window.onload = function () {
-  const main = document.getElementsByTagName("main")[0]
-  main.onclick = function changeColour() {
-    this.classList.toggle("primary");
-    this.classList.toggle("secondary");
-  }
-}
